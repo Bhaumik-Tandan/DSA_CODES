c579fc537662e8dd382a8e35d4a79e2b4b2032c8
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2021-08-06 16:42:15 +0530

    stack menu added

--- stack/array_implementaion.cpp
+++ stack/array_implementaion.cpp
@@ -8,18 +8,46 @@ void ins(int n)
 }
 int pop()
 {
-    return a[t--];
+    return a[--t];
+}
+int dispt()
+{
+    return a[t-1];
 }
 void disp()
 {
     for (int i = t - 1; i >= 0; i--)
-        cout << a[i] << endl;
+        cout << a[i] <<" ";
+}
+void menu()
+{
+    cout<<"\n1)Push\n2)Pop\n3)Display top\n4)Display whole stack\n5)Exit\nEnter your choice: ";
+    int c;
+    cin>>c;
+    switch(c)
+    {
+        case 1:
+        cout<<"\nEnter the number to be pushed: ";
+        int n;
+        cin>>n;
+        ins(n);
+        break;
+        case 2:
+        cout<<"\nYour element is: "<<pop();
+        break;
+        case 3:
+        cout<<"\nTop: "<<dispt();
+        break;
+        case 4:
+        cout<<"\nThe stack is: ";
+        disp();
+        break;
+        default:
+        exit(0);
+    }
+    menu();
 }
 main()
 {
-    ins(1);
-    ins(2);
-    ins(3);
-    pop();
-    disp();
+    menu();
 }
